f91c1faf9bdfdb53821a6514c6513e33e90909f2
hadesxiong <fengshixiong890614@gmail.com>
2024-10-15 14:09:28 +0800

    update genHtml.py

--- utils/app_feeds/genHtml.py
+++ utils/app_feeds/genHtml.py
@@ -1,9 +1,22 @@
 # coding=utf8
-import json,os
+import json,os,requests
 
 from string import Template
 from datetime import datetime
 from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
+from urllib.parse import urlparse,unquote
+
+def get_img_extension(url):
+
+    # 解析URL并提取路径
+    parsed_url = urlparse(url)
+    path = unquote(parsed_url.path)  # 解码路径，以防有特殊字符
+    
+    # 提取文件扩展名
+    _, ext = os.path.splitext(path)
+    return ext.lower()
 
 def gen_html(html_tmpl:Template,
              con_tmpl:str,
@@ -12,33 +25,68 @@ def gen_html(html_tmpl:Template,
 
     container_data = ''
 
-    for index,item in enumerate(feeds_data):
+    sorted_data = sorted(feeds_data, key=lambda x: (x['background_image'] is not None, x['background_image']), reverse=True)[:10]
+
+    for index,item in enumerate(sorted_data):
 
         ban_data = ''
         ban_before_data = ''
         ban_after_data = ''
+    
+        if item.get('background_image',None):
 
-        if index == 0:
+            try:
 
-            if item.get('background_image',None):
+                img_response = requests.get(item.get('background_image'))
 
-                ban_data = '''
-                <div class=\"ban first_ban\" style=\"background-image:url({image});\"></div>\n
-                '''.format(image=item['background_image'])
+                if img_response.status_code == 200:
+                    img_data = BytesIO(img_response.content)
+                    img_file = Image.open(img_data)
+                    
+                    if img_file.mode == 'RGB':
+                        img_file = img_file.convert('RGBA')
 
-        elif index != 0 and index % 2 == 0:
+                    # 调整尺寸
+                    origin_width, origin_height = img_file.size
+                    new_width = 824
+                    new_height = int((origin_height/origin_width)*new_width)
+                    resized_img = img_file.resize((new_width, new_height))
+                    
+                    file_ext = get_img_extension(item.get('background_image'))[1:]
+                    img_filePath = f'{target_dir}/img/{item["entry_id"]}.{file_ext.lower()}'
 
-            if item.get('background_image',None):
+                    if file_ext in ['jpg', 'jpeg','png','JPG','JPEG','PNG']:
+                        resized_img.save(img_filePath,'PNG',quality=85)
+                        
+                    else:
+                        resized_img.save(img_filePath,file_ext.upper())
 
-                ban_after_data = '''
-                <div class=\"ban\" style=\"background-image:url({image});\"></div>\n
-                '''.format(image=item['background_image'])
-        else:
+                    item['background_image'] = img_filePath
 
-            if item.get('background_image',None):
+                else:
+                    item['background_image'] = None
+
+            except Exception as e:
+                item['background_image'] = None
+                print('save fail',item['background_image'],item['entry_id'])
+
+        
+        if index == 0 and item.get('background_image',None):
+
+            ban_data = '''
+            <div class=\"ban first_ban\" style=\"background-image:url(file://{image});\"></div>\n
+            '''.format(image=item['background_image'])
+
+        elif index != 0 and index % 2 == 0 and item.get('background_image',None):
+
+            ban_after_data = '''
+            <div class=\"ban\" style=\"background-image:url(file://{image});\"></div>\n
+            '''.format(image=item['background_image'])
+
+        elif item.get('background_image',None):
 
                 ban_before_data = '''
-                <div class=\"ban\" style=\"background-image:url({image});\"></div>\n
+                <div class=\"ban\" style=\"background-image:url(file://{image});\"></div>\n
                 '''.format(image=item['background_image'])
 
         each_container = Template(con_tmpl).substitute(
