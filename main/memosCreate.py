4f9518303f8ec7eaaa72481801a4344e6e4434ad
hadesxiong <fengshixiong890614@gmail.com>
2024-11-11 10:01:46 +0800

    update memosCreate.py

--- main/memosCreate.py
+++ main/memosCreate.py
@@ -3,6 +3,7 @@ import os,sys,requests
 # 添加引用路径
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
 
+from datetime import datetime
 from app_memos.planParse import create_plan_daily
 
 # 读取环境变量
@@ -10,18 +11,26 @@ memos_url = os.getenv('app_memos_api_url')
 memos_token = os.getenv('app_memos_token')
 memos_daily_plan = os.getenv('app_memos_dailyPlan')
 
-plan_content = create_plan_daily(memos_daily_plan)
+today = datetime.today().weekday()
 
-memos_headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {memos_token}'
-}
+if today <= 4:
 
-memos_data = {'content': plan_content, 'visibility':'PUBLIC'}
+    plan_content = create_plan_daily(memos_daily_plan)
 
-memos_rslt = requests.post(f'{memos_url}/api/v1/memo',json = memos_data, headers= memos_headers)
+    memos_headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {memos_token}'
+    }
 
-if memos_rslt.status_code == 200:
-    print('创建成功')
+    memos_data = {'content': plan_content, 'visibility':'PUBLIC'}
+
+    memos_rslt = requests.post(f'{memos_url}/api/v1/memo',json = memos_data, headers= memos_headers)
+
+    if memos_rslt.status_code == 200:
+        print('创建成功')
+    else:
+        print('创建失败')
+
 else:
-    print('创建失败')
+
+    print('今天是周末，系统不自动创建任务')
